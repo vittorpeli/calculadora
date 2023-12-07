465e80be10666a4c08b3e4b2e5076fc6c6e26501
vittor <vittor.pelicarto137@gmail.com>
2023-12-07 00:30:35 -0300

    update index.js

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map(button => {
-  button.addEventListener('click', (e) => {
-    switch(e.target.innerText) {
-      case 'C':
-        display.innerText = '';
-        break;
-      case '=':
-        try{
-          display.innerText = eval(display.innerText);
-        } catch {
-          display.innerText = "Error"
-        }
-        break;
-      case 'DEL':
-        if(display.innerText) {
-          display.innerText = display.innerText.slice(0, -1);
-        }
-        break;
-      default: 
-        display.innerText += e.target.innerText;
-    }
-  })
+  button.addEventListener('click', (e) => handleButtonClick(e.target.innerText))
 })
+
+document.addEventListener('keydown', (e) => {
+  const keyPressed = e.key
+
+  const button = buttons.find(btn => btn.innerText === keyPressed)
+
+  if(button) {
+    handleButtonClick(keyPressed)
+  }
+})
+
+function handleButtonClick(value) {
+  switch(value) {
+    case 'C':
+      display.innerText = '';
+      break;
+    case '=':
+      try{
+        display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Error"
+      }
+      break;
+    case 'DEL':
+      if(display.innerText) {
+        display.innerText = display.innerText.slice(0, -1);
+      }
+      break;
+    default: 
+      display.innerText += value;
+  }  
+}
+
